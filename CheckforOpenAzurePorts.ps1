2c4b680d1570e6cb8a9a0faf2660ff485e35f52e
John Morehouse <john@jmorehouse.com>
2019-03-06 09:07:15 -0500

    Create CheckforOpenAzurePorts.ps1
    initial check in for script that will check NSG's for open ports

---
+++ CheckforOpenAzurePorts.ps1
@@ -0,0 +1,19 @@
+﻿$path = "c:\temp\openports.txt"
+
+#log into Azure Account if needed
+IF (-NOT (get-azcontext)){
+    Login-AzAccount
+}
+
+#get all of the subs
+$subs = get-azsubscription
+foreach($sub in $subs){
+    set-azcontext -SubscriptionID $sub.ID
+    $nsgs = Get-AzNetworkSecurityGroup 
+        foreach($nsg in $nsgs){
+            $nsg | get-aznetworksecurityruleconfig | where-object -FilterScript {$_.sourceaddressprefix -eq "*" -and $_.Access -eq "Allow"} | out-file -FilePath $path -Append
+    }
+}
+
+#open the file
+invoke-item $path
