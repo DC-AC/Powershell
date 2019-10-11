f24bfa143bb0a1d8cc0ed3535a8b49156b77a3ba
John Morehouse <john@jmorehouse.com>
2019-10-11 14:56:41 -0400

    Create Clean up Azure blob storage.ps1
    Initial create

---
+++ Clean up Azure blob storage.ps1
@@ -0,0 +1,30 @@
+##########################################################################################
+#- Author: John Morehouse
+# Date: July 2019
+# B: https://sqlrus.com
+# W: https://dcac.com
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+# AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# IN OTHER WORDS: USE AT YOUR OWN RISK.
+#
+# AUTHOR ASSUMES ZERO LIABILITY OR RESPONSIBILITY.
+#
+##########################################################################################
+#Import-Module -Name AzureRM
+
+$SAS = ""
+
+#forcing TLS1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+
+#set cleanup time to 31 days (in hours). The will ensure files are deleted well after 30 days
+$CleanupTime = [DateTime]::UtcNow.AddHours(-744)
+
+#get a list of blobs that need to be trashed
+$context = New-AzureStorageContext -StorageAccountName "acumensqlbackups" -SasToken $SAS
+$x = Get-AzureStorageBlob -Container "sqlbackups-container" -Context $context | Where-Object { $_.LastModified.UtcDateTime -lt $CleanupTime} # | Remove-AzureStorageBlob
+
+# do the dirty work
+$x | Remove-AzureStorageBlob
