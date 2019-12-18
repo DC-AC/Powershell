f622637af62a504ff44dca53eaabb69ae3e88070
jdanton <jdanton1@yahoo.com>
2019-12-18 15:35:09 -0800

    Update DiskSpdParser.ps1

--- DiskSpdParser.ps1
+++ DiskSpdParser.ps1
@@ -1,5 +1,4 @@
-
-$path='$InsertYourPathHere'
+$path='$YourPathHere'
 
 $Files = Get-ChildItem -Path $path
 
@@ -7,10 +6,12 @@ foreach ($file in $files)
 {  
    $content = get-content $file
 
-   $command= ($content)|select -first 1 -skip 1
-   $results= ($content)|grep total -m1|sed 's/"|"/","/g'|sed 's/"total:"//g'
+   $command = ($content)|select -first 1 -skip 1
+   $results = ($content)|grep total -m1|sed 's/"|"/","/g'|sed 's/"total:"//g'
+   $maxlatency = ($content)|grep 99th|sed 's/|/,/g'
  
    $results= $results.split(",")
+   $maxlatency = $maxlatency.split(",")
 
    $Output = New-Object -TypeName PSObject -Property @{
       FileName = $File.Name
@@ -20,7 +21,11 @@ foreach ($file in $files)
       MiBperSec = $results[2].Trim()
       IOPs = $results[3].Trim()
       AvgLatency = $results[4].Trim()
-      LatStdDev = $results[5].Trim()}| Select-Object FileName,Command,TotalBytes, TotalIOs, MiBperSec, IOPs,AvgLatency,LatStdDev
+      LatStdDev = $results[5].Trim()
+      Perecentile = $maxlatency[0].Trim()
+      ReadMS = $maxlatency[1].Trim()
+      WriteMS = $maxlatency[2].Trim()
+      TotalMS = $maxlatency[3].Trim()}| Select-Object FileName,Command,TotalBytes, TotalIOs, MiBperSec, IOPs,AvgLatency,LatStdDev,Percentile,ReadMS,WriteMS,TotalMS
 
       $Output|Export-CSV results.csv -Append
 }
